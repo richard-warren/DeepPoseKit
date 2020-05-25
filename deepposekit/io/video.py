646b4bb9cc7fc4b9c9733708c1e86912d9534bbc
rwarren2163 <richardwarren2163@gmail.com>
2020-05-25 10:10:40 -0400

    wip

--- deepposekit/io/video.py
+++ deepposekit/io/video.py
@@ -17,6 +17,7 @@ from tensorflow.python.keras.utils import Sequence
 import cv2
 import numpy as np
 import os
+import ipdb
 
 __all__ = ["VideoReader", "VideoWriter"]
 
@@ -35,7 +36,9 @@ class VideoReader(cv2.VideoCapture, Sequence):
         If gray, return only the middle channel
     """
 
-    def __init__(self, videopath, batch_size=1, gray=False, pad_imgs=False):
+    # todo: add option of specifying dimensions to crop/pad to // ensure padding doesn't occur if frame is already correct dimensions
+
+    def __init__(self, videopath, batch_size=1, gray=False, frame_size=None):
 
         if isinstance(videopath, str):
             if os.path.exists(videopath):
@@ -54,14 +57,11 @@ class VideoReader(cv2.VideoCapture, Sequence):
         self.finished = False
         self.gray = gray
         self._read = super(VideoReader, self).read
-        self.pad_imgs = pad_imgs  # whether to pad the images so all dimensions are continuously divisible by 2
+        self.frame_size = frame_size  # optional size frame size // to re-size, frames will be either cropped or padded with zeros
 
-        if self.pad_imgs:
-            self.valid_dims = [2, 4, 6, 8, 10, 12, 14, 16, 20, 22, 24, 28, 32, 40, 44, 48, 56, 64, 80, 88, 96, 112, 128, 160, 176, 192, 224, 256, 320, 352, 384, 448, 512, 640, 704, 768, 896, 1024, 1280, 1408, 1536, 1792, 2048, 2560, 2816, 3072, 3584, 4096, 5120, 5632, 6144, 7168, 10240, 11264, 12288, 14336, 20480, 22528, 28672, 45056]
-            self.height_padded = list(filter(lambda i: i >= self.height, self.valid_dims))[0]
-            self.width_padded = list(filter(lambda i: i >= self.width, self.valid_dims))[0]
-            print('frames will be padded to from (%i, %i) to (%i, %i)' % (self.height, self.width, self.height_padded, self.width_padded))
-
+        if self.frame_size is not None:
+            print('frames will be re-sized to (%i, %i)' % (self.frame_size[0], self.frame_size[1]))
+        
     def read(self):
         """ Read one frame
 
@@ -76,9 +76,12 @@ class VideoReader(cv2.VideoCapture, Sequence):
         if ret:
             self.idx += 1
 
-            if self.pad_imgs:
-                frame = np.concatenate((frame, np.zeros((self.height_padded - self.height, self.width, 3))), axis=0)
-                frame = np.concatenate((frame, np.zeros((self.height_padded, self.width_padded - self.width, 3))), axis=1)
+            if self.frame_size is not None:
+                frame_resized = np.zeros((self.frame_size[0], self.frame_size[1],3), dtype='uint8')
+                x = min(frame.shape[1], self.frame_size[1])
+                y = min(frame.shape[0], self.frame_size[0])
+                frame_resized[:y,:x] = frame[:y,:x]
+                frame = frame_resized
 
             if self.gray:
                 frame = frame[..., 1][..., None]
